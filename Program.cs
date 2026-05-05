025b55f938f628230baef88fe58df4cc6871f91d
IchigoStarmiya <acciowand200@hotmail.com>
2026-05-05 23:38:20 +0200

    heartbeat timer on jungle bot

--- Program.cs
+++ Program.cs
@@ -1,18 +1,31 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using NetCord.Gateway;
 using NetCord.Hosting.Gateway;
 using NetCord.Hosting.Services;
 using NetCord.Hosting.Services.ApplicationCommands;
+using VoiceTimerBot.BackgroundServices;
 using VoiceTimerBot.Interfaces;
 using VoiceTimerBot.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.Logging.AddSentry(options =>
+{
+    options.Dsn = builder.Configuration["Dsn"];
+    options.MinimumEventLevel = LogLevel.Error;
+    options.Environment = builder.Environment.EnvironmentName;
+    options.AutoSessionTracking = true;
+    options.Debug = builder.Environment.IsDevelopment();
+});
+
 builder.Services.Configure<VoiceTimerSettings>(
     builder.Configuration.GetSection("VoiceTimer"));
 
 builder.Services.AddSingleton<IVoiceTimerService, VoiceTimerService>();
+
+builder.Services.AddHostedService<SentryHeartbeatService>();
 
 builder.Services
        .AddDiscordGateway(options =>
